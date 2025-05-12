c267cf38b89af28c3cfe3bee5774fb1941ca8b14
marcosantos <marcodossantoscontato@gmail.com>
2025-05-12 10:54:46 -0300

    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
+
 }
 
 android {
     namespace = "com.devspace.scribblespace"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.devspace.scribblespace"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 26
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -78,7 +79,6 @@ dependencies {
 
     //Room database
     implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
     implementation("androidx.room:room-guava:$room_version")
--- app/src/main/java/com/devspace/scribblespace/common/remote/RemoteDataSource.kt
+++ app/src/main/java/com/devspace/scribblespace/common/remote/RemoteDataSource.kt
@@ -3,29 +3,87 @@ package com.devspace.scribblespace.common.remote
 import com.devspace.scribblespace.common.model.NoteData
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
-import java.lang.Exception
+import kotlinx.coroutines.withTimeout
+import kotlin.Exception
 
 private const val NOTE_COLLECTION = "notes"
 
 class RemoteDataSource private constructor(
-
+    private val database: FirebaseFirestore
 ) {
 
     suspend fun addNote(title: String, description: String): Result<String> {
-      return TODO()
+        return try {
+            val noteMap = hashMapOf(
+                "title" to title,
+                "description" to description
+            )
+
+            val addedDocument = withTimeout(2000) {
+                database.collection(NOTE_COLLECTION)
+                    .add(noteMap)
+                    .await()
+            }
+
+            Result.success(addedDocument.id)
+        } catch (ex: Exception) {
+            Result.failure(ex)
+        }
     }
+
 
     suspend fun getNotes(): Result<List<NoteData>> {
-       return TODO()
+        return try {
+            //Snapshot query
+            val querySnapshot = database
+                .collection(NOTE_COLLECTION)
+                .get()
+                .await()
+
+            //Get notes
+            val notesFromRemote = querySnapshot.documents.mapNotNull { noteFromDb ->
+                noteFromDb.toObject(NoteRemoteData::class.java)
+                    ?.copy(id = noteFromDb.id)
+            }
+
+            val notesData: MutableList<NoteData> = mutableListOf()
+            notesFromRemote.forEach { note ->
+                if (note.id != null && note.title != null && note.description != null) {
+                    notesData.add(
+                        NoteData(
+                            key = note.id,
+                            title = note.title,
+                            description = note.description
+                        )
+                    )
+                }
+            }
+            Result.success(notesData)
+        } catch (ex: Exception) {
+            Result.failure(ex)
+        }
     }
 
+
     suspend fun deleteNote(id: String): Result<Unit> {
-      return TODO()
+        return try {
+            withTimeout(2000) {
+
+
+                database.collection(NOTE_COLLECTION)
+                    .document(id)
+                    .delete()
+                    .await()
+                Result.success(Unit)
+            }
+        } catch (ex: Exception) {
+            Result.failure(ex)
+        }
     }
 
     companion object {
         fun create(): RemoteDataSource {
-            return RemoteDataSource()
+            return RemoteDataSource(database = FirebaseFirestore.getInstance())
         }
     }
 }
